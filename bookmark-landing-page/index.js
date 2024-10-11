808988a897deabae1d26e6a42e1f5443b0f6829d
Mateus <mtsmlima@gmail.com>
2024-10-11 16:46:36 -0300

    Added logic to click on a link and the right section appear

--- bookmark-landing-page/index.js
+++ bookmark-landing-page/index.js
@@ -6,6 +6,51 @@ const hamburguerMenu = document.querySelector('.header__menu-hamburguer')
 
 const media = window.matchMedia('(min-width: 48rem)')
 
+const sections = document.querySelectorAll('.section')
+
+const links = document.querySelectorAll('.features__link')
+
+
+
+// Wanted to make with classic for and forEach()
+
+// for (let i = 0; i < links.length; i++) {
+//    links[i].addEventListener('click', (e) => {
+//       e.preventDefault()
+
+//       for (let section = 0; section < sections.length; section++) {
+//          sections[section].classList.remove('active')
+//       }
+
+//       for (let i = 0; i < links.length; i++) {
+//          links[i].classList.remove('selected')
+//       }
+
+//       sections[i].classList.add('active')
+
+//       links[i].classList.add('selected')
+//    })
+// }
+
+links.forEach( (link, index) => {
+   link.addEventListener('click', (event) => {
+
+      // This is used so the link doesn't 'go' anywhere.
+      // In this project, the href='##' so it's ok, I left for future reference.
+      event.preventDefault()
+
+      for (let section of sections) {
+         section.classList.remove('active')
+      }
+      
+      links.forEach(link => link.classList.remove('selected'))
+
+      sections[index].classList.add('active')
+
+      link.classList.add('selected')
+   })
+})
+
 
 function openMenu() {
    hamburguerMenu.classList.toggle('is-open')
@@ -18,13 +63,10 @@ function closeMenu() {
    hamburguerMenu.classList.toggle('is-open')
    hamburguerButton.setAttribute('aria-expanded', 'false')
 
-   setTimeout( () => {
+   setTimeout(() => {
       hamburguerMenu.removeAttribute('style')
    }, 500)
-
 }
-
-
 hamburguerButton.addEventListener('click', openMenu)
 
 closeButton.addEventListener('click', closeMenu)
@@ -37,7 +79,9 @@ function checkViewportSize() {
       hamburguerMenu.style.transition = ''
    }
 }
-
 checkViewportSize()
-
 window.addEventListener('resize', checkViewportSize);
+
+
+sections[0].classList.add('active')
+links[0].classList.add('selected')
